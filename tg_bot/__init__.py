35fc322b2b210dde845f564fd972db0f89c65075
AlexyYakimov <smugastblu@bk.ru>
2023-05-13 10:45:28 +0300

    fix weather api

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -1,11 +1,11 @@
 from telebot.types import ReplyKeyboardMarkup, InlineKeyboardMarkup
 
+from in_memory_db.in_memory_data import my_id
 from tg_bot import bot, handlers
 from tg_bot import keyboards
 from tg_bot.bot import bot as main_bot
 from tg_bot.keyboards import aqi_btn, puk_btn, inline_aqi_btn, weather_btn, custom_amount_btn
 from tg_bot.utils import get_webhook_url
-from in_memory_db import in_memory_data
 from in_memory_db.token_storage import Token, get_token
 
 
@@ -33,4 +33,4 @@ def set_webhook():
     main_bot.remove_webhook()
     main_bot.set_webhook(url=ngrok_url)
 
-    bot.send_message(global_utils.my_id, f"Bot started {ngrok_url} \n\n Alert: {get_token(Token.ALERT)}")
+    bot.send_message(my_id, f"Bot started {ngrok_url} \n\n Alert: {get_token(Token.ALERT)}")
