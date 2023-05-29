95e9db7fbda45529bf21d5be3d0e21a1071f496d
AlexyYakimov <smugastblu@bk.ru>
2023-05-29 14:59:21 +0300

    fix korona service

--- korona_service/korona_api.py
+++ korona_service/korona_api.py
@@ -24,14 +24,14 @@ locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 max_lari_cap = 4950
 
 
-def get_rate_for(amount: int = 4000) -> dict[str, float]:
+def get_rate_for(amount: int) -> dict[str, float]:
     query_parameters['receivingAmount'] = amount * 100
     data = requests.get(url=url, params=query_parameters, headers=headers).json()[0]
     sending_amount = data['sendingAmountWithoutCommission'] / 100
     return {'exchangeRate': data['exchangeRate'], 'amount': sending_amount}
 
 
-def get_custom_amount(amount: int = 4000) -> str:
+def get_custom_amount(amount: int) -> str:
     try:
         data = get_rate_for(amount)
         return f"Exchange Rate: <b>{data['exchangeRate']}</b>\n\n" \
@@ -39,9 +39,11 @@ def get_custom_amount(amount: int = 4000) -> str:
     except RequestException:
         print("Error")
         return "Some thing went wrong, try again later!"
-    except ValueError:
+    except ValueError as e:
+        print(f"Value Error {e}")
         return f"Wou wou wou, easy, enter sum less then <b>{max_lari_cap}₾</b>"
-    except KeyError:
+    except KeyError as e:
+        print(f"Key Error {e}")
         return f"Wou wou wou, easy, enter sum less then <b>{max_lari_cap}₾</b>"
 
 
