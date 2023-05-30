28b5d29598c54f12a95c75c963706c40755e4b45
AlexyYakimov <smugastblu@bk.ru>
2023-05-31 00:39:54 +0300

    fix db

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,5 @@
 from db.queues import fill_currency_name
-from db.tables import create_currency_db, create_rates_db, drop_db, create_korona_table
+from db.tables import create_currency_db, create_rates_db, create_korona_table
 
 
 def init_db_tables():
