54a3c4845aabfdb89c5f078c3315fa934ffd0122
AlexyYakimov <smugastblu@bk.ru>
2023-07-24 21:06:50 +0400

    fix scheduller

--- schedule_runner/sheduler.py
+++ schedule_runner/sheduler.py
@@ -4,6 +4,8 @@ from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR, EVENT_JOB_MA
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 
+from tg_bot.bot import send_log_message
+
 flask = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
@@ -20,13 +22,13 @@ def add_task(func, hours, minute):
                       hour=hours,
                       minute=minute,
                       timezone='Asia/Tbilisi',
+                      misfire_grace_time= 20000,
                       replace_existing=True)
 
 
 def my_listener(event):
     if event.exception:
-        print('The job crashed :(')
-        print(f"{scheduler.get_jobs()}")
+        send_log_message(0, "Job crashed!!")
     else:
         print('The job worked :)')
 
