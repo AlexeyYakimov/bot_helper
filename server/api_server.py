bf3dd8d8b3fc5d558905ab55d97ee499a4a10f3f
AlexyYakimov <smugastblu@bk.ru>
2023-11-04 22:01:48 +0400

    add application/json

--- server/api_server.py
+++ server/api_server.py
@@ -1,3 +1,9 @@
 from flask import Flask
 
 flask_app = Flask(__name__)
+
+
+@flask_app.after_request
+def add_header(response):
+    response.headers['Content-type'] = 'application/json'
+    return response
