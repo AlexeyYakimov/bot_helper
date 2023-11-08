be30532343eecc0c7f1370b06ad9a8b5663b78de
AlexyYakimov <smugastblu@bk.ru>
2023-11-09 00:10:22 +0400

    add ping method

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import weather_service
 from db import init_db_tables
 from korona_service.api import korona_api_routes
 from schedule_runner import add_all_tasks, run_cron, clean_cron
-from server.api_server import flask_app
+from server.api_server import flask_app, server_api_routes
 from tg_bot.api import bot_api_routes
 from weather_service.api import weather_api_routes
 
@@ -19,6 +19,7 @@ if __name__ == '__main__':
 
     tg_bot.set_webhook()
 
+    flask_app.register_blueprint(server_api_routes)
     flask_app.register_blueprint(bot_api_routes)
     flask_app.register_blueprint(korona_api_routes)
     flask_app.register_blueprint(weather_api_routes)
--- server/api_server.py
+++ server/api_server.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, Blueprint, request
+
+from in_memory_db.token_storage import get_token, Token
 
 flask_app = Flask(__name__)
+
+server_api_routes = Blueprint('server_api', __name__, url_prefix='/tech')
+
+# curl -H "Authorization: $API_TOKEN" https://$PROXY_DOMAIN.loca.lt/tech/ping
+
+
+@server_api_routes.get("/ping")
+def ping_server():
+    if request.headers['Authorization'] == get_token(Token.API):
+        return 200
+    else:
+        return 401
 
 
 @flask_app.after_request
 def add_header(response):
     response.headers['Content-type'] = 'application/json'
     return response
+
